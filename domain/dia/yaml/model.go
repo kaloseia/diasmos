0653749c55a89b30c98950578eeb6888ebd8efc2
bnunamak <brennan@nunamaker.de>
2023-09-19 15:58:51 +0200

    Added helper method for `yaml.Model` to get identifier field definitions, test entity

--- domain/dia/yaml/model.go
+++ domain/dia/yaml/model.go
@@ -1,8 +1,25 @@
 package yaml
 
+import "log"
+
 type Model struct {
 	Name        string                   `yaml:"name"`
 	Fields      map[string]Field         `yaml:"fields"`
 	Identifiers map[string]Identifier    `yaml:"identifiers"`
 	Related     map[string]ModelRelation `yaml:"related"`
+}
+
+func (m *Model) GetIdentifierFields() []Field {
+	var fields []Field
+	for _, identifier := range m.Identifiers {
+		for _, fieldName := range identifier.Fields {
+			idField, fieldExists := m.Fields[fieldName]
+			if !fieldExists {
+				log.Printf("identifier field '%s' does not exist in model '%s'", fieldName, m.Name)
+				continue
+			}
+			fields = append(fields, idField)
+		}
+	}
+	return fields
 }
